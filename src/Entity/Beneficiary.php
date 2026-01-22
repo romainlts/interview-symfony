05fba526479783776eeac80453274c67deaa1206
Romain Lts <70719198+romainlts@users.noreply.github.com>
2026-01-22 18:10:14 +0100

    fix(Beneficiary): update ApiResource namespace for Symfony 7.3 compatibility

--- src/Entity/Beneficiary.php
+++ src/Entity/Beneficiary.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\BeneficiaryRepository;
 use Doctrine\ORM\Mapping as ORM;
 
