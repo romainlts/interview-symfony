f2480dd6159585b6e439523dc17b9297da0afe2c
Romain Lts <70719198+romainlts@users.noreply.github.com>
2026-01-22 17:23:00 +0100

    feat(dashboard): add client-side search for non-persisted beneficiaries

---
+++ public/assets/js/dashboard.js
@@ -0,0 +1,42 @@
+/* ------------------------------------------------------------------------------
+ *
+ *  # Dashboard page
+ *
+ * ---------------------------------------------------------------------------- */
+
+
+// Setup module
+// ------------------------------
+function SearchNonPersistedBeneficiaries() {
+    const searchBar = document.getElementById('search-non-persisted');
+    const beneficiaries = document.querySelectorAll('.beneficiary-non-persisted');
+
+    searchBar.addEventListener('input', (e) => {
+        const search = e.target.value.toLowerCase();
+        
+        beneficiaries.forEach(beneficiary => {
+            const name = beneficiary.dataset.name;
+                if (name.includes(search)) {
+                    beneficiary.style.opacity = '0';
+                    beneficiary.style.display = '';
+                    setTimeout(() => {
+                        beneficiary.style.transition = 'opacity 0.3s ease-in-out';
+                        beneficiary.style.opacity = '1';
+                    }, 10);
+                } else {
+                    beneficiary.style.transition = 'opacity 0.3s ease-in-out';
+                    beneficiary.style.opacity = '0';
+                    setTimeout(() => {
+                        beneficiary.style.display = 'none';
+                    }, 300);
+                }
+        });
+    });
+}
+
+
+// Initialize module
+// ------------------------------
+document.addEventListener('DOMContentLoaded', function() {
+    SearchNonPersistedBeneficiaries();
+});
