6b4349e4748732e8b7690f27bb851989dcba0484
Romain Lts <70719198+romainlts@users.noreply.github.com>
2026-01-22 16:52:30 +0100

    feat(dashboard): display 12 non-persisted random beneficiaries

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,17 +5,35 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Faker\Generator;
 
 final class DashboardController extends AbstractController
 {
     /**
      * Displays the main dashboard page
+     * Displays a list of non-persisted beneficiaries with generated avatars
      *
+     * @param Generator $faker Faker service for generating fake data
      * @return Response The rendered dashboard template
      */
     #[Route('/', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(Generator $faker): Response
     {
-        return $this->render('dashboard.html.twig');
+        // Generate non-persisted beneficiaries with avatars
+        // Dicebear avatar API endpoint with parameters for eyes and mouth styles
+        $avatarEndpoint = 'https://api.dicebear.com/8.x/avataaars/svg?eyes=hearts,happy,default,side,wink&mouth=smile,default,twinkle,serious&seed=';
+        $nonPersistedBeneficiaries = [];
+
+        for ($i = 0; $i < 12; $i++) {
+            $firstName = $faker->firstName();
+            $nonPersistedBeneficiaries[] = [
+                'firstName' => $firstName,
+                'avatarUrl' => $avatarEndpoint . urlencode($firstName),
+            ];
+        }
+
+        return $this->render('dashboard.html.twig', [
+            'nonPersistedBeneficiaries' => $nonPersistedBeneficiaries,
+        ]);
     }
 }
