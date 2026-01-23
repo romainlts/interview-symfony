3be944615f49541475c506a5b94b26f00c6808dc
Romain Lts <70719198+romainlts@users.noreply.github.com>
2026-01-24 00:23:55 +0100

    feat(api): filter beneficiaries by name

--- src/Entity/Beneficiary.php
+++ src/Entity/Beneficiary.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\BeneficiaryRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -13,6 +15,7 @@ use Symfony\Component\Validator\Constraints as Assert;
     normalizationContext: ['groups' => ['beneficiary:read']],
     denormalizationContext: ['groups' => ['beneficiary:write']]
 )]
+#[ApiFilter(SearchFilter::class, properties: ['name' => 'partial'])]
 #[ORM\Entity(repositoryClass: BeneficiaryRepository::class)]
 class Beneficiary
 {
